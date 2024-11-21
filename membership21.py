3b94bc3accf4bc311631bdcb050c8b706edd1e81
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-21 05:18:42 -0800

    index

---
+++ membership21.py
@@ -0,0 +1,16 @@
+#   Membership checking in the string------
+#   in and not in...
+
+s = "python is a high level progamming language"
+
+ss = "language"
+
+print(ss in s)
+
+if ss in s:
+    print("ss is available in s")
+if ss not in s :
+    print("ss is not available in s")
+
+
+
---
+++ string21.py
@@ -0,0 +1,20 @@
+#sting------>>
+name = 'Raviteja'
+name1 = "sai"
+name2 = "vamsi"
+
+print(type(name))
+print(type(name1))
+print(type(name2))
+
+
+
+
+
+
+
+
+
+
+
+
