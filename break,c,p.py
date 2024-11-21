186f4494e1b540c88e1b9f6d680dedb9ca124cf7
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-21 02:06:58 -0800

    break,continue,pass20

---
+++ break,c,p.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    if i == 3:
+        break  
+    print(i)
+
+    # -----continue-------
+for x in range(5):
+    if x == 3:
+        continue  
+    print(x)
+for y in range(10):
+    if y == 6:
+        continue
+    print(y)
+    #---------pass--------
+for z in range(6):
+    if z == 3:
+        pass
+    print(z)
+    
