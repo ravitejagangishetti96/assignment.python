0f8ba7b37a993eaddeaa068a5a72d5f8f46ea59a
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-25 07:02:59 -0800

    check type of charecter

---
+++ checkchr2511124.py
@@ -0,0 +1,32 @@
+#check type of a charecter
+
+statement=input("enter the statement")
+
+#isupper
+print("-------------------")
+print(statement.isupper())
+
+#islower
+print("--------------------")
+print(statement.islower())
+
+#istitle
+print("---------------------")
+print(statement.istitle())
+
+#isalnum
+print("------------------------")
+print(statement.isalnum()) 
+
+#isdigit
+print("----------------------")
+print(statement.isdigit())
+
+#isspace
+print("-----------------------")
+print(statement.isspace())
+
+
+
+ 
+
