30ed98f62e8605d301873632c78f2bf2bafbb5a8
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-25 04:01:52 -0800

    find,index

---
+++ find251124.py
@@ -0,0 +1,17 @@
+#finding of substring in main string
+
+#find----------------
+
+statement=input("enter your statement")
+
+print(statement.find("done"))
+print(statement.find("python"))
+
+#index------
+
+print("-------------------------")
+
+print(statement.index("word"))
+print(statement.index("learing"))
+
+
