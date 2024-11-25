47807f6b927098abdb1961ad38d8f15126e91db3
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-25 06:29:41 -0800

    change case of string

---
+++ case251124.py
@@ -0,0 +1,24 @@
+#changing case of a string
+
+statement=input("enter the statement here")
+
+#uppercase
+print("-----------------------------")
+print(statement.upper())
+
+#lowercase
+print("-------------------------------")
+print(statement.lower())
+
+#swapcase
+print("--------------------")
+print(statement.swapcase()) 
+
+#title
+print("---------------------")
+print(statement.title())
+
+#capitalize
+print("------------------------")
+print(statement.capitalize())
+
