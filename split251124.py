14ed24be1f0fb1680f21c2c53454dfecfd65d920
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-11-25 04:43:06 -0800

    split,join

---
+++ split251124.py
@@ -0,0 +1,17 @@
+
+massage=input("give your massege here")
+
+#split
+
+print(len(massage))
+
+submassage=massage.split("")
+
+print(len(submassage))
+print(submassage)
+
+#join
+print("---------------------")
+joinmassage=" ".join(massage)
+print(len(joinmassage))
+print(joinmassage)
