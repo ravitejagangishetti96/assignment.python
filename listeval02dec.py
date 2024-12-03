65be915f6146481914dd2eb96edcf98e8f82bc64
ravitejagangishetti96 <ravitejashetti96@gmail.com>
2024-12-03 03:18:24 -0800

    index,slicing,eval

--- listeval02dec.py
+++ listeval02dec.py
@@ -7,9 +7,10 @@ print(type(l))
 print(type(l[1]))
 
 #list without using eval
+print("------------------------")
 
 num=input("enter your list:")
-l=num.split('1')
+l=num.split(",")
 print(l[0])
 print(type(l))
 
---
+++ listinsl02dec.py
@@ -0,0 +1,10 @@
+#accesing elements from list
+
+# indexing
+l=[1,2,3,4,5,6,7,8,9]
+print(l[5])
+
+#slicing
+print("---------------------")
+print(l[1:9])   #[start index : end index]
+print(l[1:9:3])  #[start index : end index : step]
